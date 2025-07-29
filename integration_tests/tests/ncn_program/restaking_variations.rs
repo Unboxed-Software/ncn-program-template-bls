e3b681c3744763ef07d349b37a70aa320a5ab7b7
MohammedAlabd <mu96abd@gmail.com>
2025-07-29 21:02:05 +0300

    fix removing_operator/vault test

--- integration_tests/tests/ncn_program/restaking_variations.rs
+++ integration_tests/tests/ncn_program/restaking_variations.rs
@@ -2,7 +2,8 @@
 mod tests {
 
     use jito_restaking_core::MAX_FEE_BPS;
-    use ncn_program_core::error::NCNProgramError;
+    use ncn_program_core::{epoch_snapshot, error::NCNProgramError};
+    use solana_sdk::msg;
 
     use crate::fixtures::{
         ncn_program_client::assert_ncn_program_error, test_builder::TestBuilder, TestResult,
@@ -39,7 +40,9 @@ mod tests {
         }
 
         {
-            fixture.snapshot_test_ncn(&test_ncn).await?;
+            fixture
+                .update_snapshot_test_ncn_new_epoch(&test_ncn)
+                .await?;
             fixture.vote_test_ncn(&test_ncn).await?;
         }
 
@@ -50,6 +53,7 @@ mod tests {
     async fn test_removing_vault() -> TestResult<()> {
         let mut fixture = TestBuilder::new().await;
         let mut restaking_client = fixture.restaking_program_client();
+        let mut ncn_program_client = fixture.ncn_program_client();
 
         const OPERATOR_COUNT: usize = 1;
         const INDEX_OF_VAULT_TO_REMOVE: usize = 0;
@@ -80,11 +84,24 @@ mod tests {
             fixture.warp_epoch_incremental(1).await?;
         }
 
+        let epoch_snapshot = ncn_program_client
+            .get_epoch_snapshot(test_ncn.ncn_root.ncn_pubkey)
+            .await?;
+        msg!("Epoch Snapshot before: {}", epoch_snapshot);
+
         {
             // Second Run
-            fixture.snapshot_test_ncn(&test_ncn).await?;
+            fixture
+                .update_snapshot_test_ncn_new_epoch(&test_ncn)
+                .await?;
 
-            fixture.vote_test_ncn(&test_ncn).await?;
+            let epoch_snapshot = ncn_program_client
+                .get_epoch_snapshot(test_ncn.ncn_root.ncn_pubkey)
+                .await?;
+            msg!("Epoch Snapshot after: {}", epoch_snapshot);
+
+            let result = fixture.vote_test_ncn(&test_ncn).await;
+            assert_ncn_program_error(result, NCNProgramError::OperatorHasNoMinimumStake, Some(1));
         }
 
         Ok(())
